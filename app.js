8aad1f5e27da447d6e2149abc9aedd7b4118a595
Mahfod <mahfod.dev@gmail.com>
2022-03-07 19:37:26 +0100

    change port

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
 	res.send('Hello from yelp')
 })
 
+app.get('/api/', (req, res) => {
+	res.send('Hello from yelp')
+})
+
 app.listen(8000, () => {
 	console.log('all is good')
 })
